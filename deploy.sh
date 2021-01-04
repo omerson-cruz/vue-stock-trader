797264565e6aa2bbd388671a377c55cf6e326524
omerson-cruz <omerson.e.cruz@gmail.com>
2021-01-04 12:53:54 +0800

    Changed config

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git push -f git@github.com:omerson-cruz/vue-gh-page.git master:gh-pages
+git push -f git@github.com:omerson-cruz/vue-stock-trader master:gh-pages
 
 cd -
